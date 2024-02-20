f5601a522c475e72f1f9093b47a6d11a5bc196f9
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-02-20 16:29:15 +0530

    init: payload configuration

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import { getPayloadClient } from './get-payload';
 
 const PORT = Number(process.env.PORT) || 3000;
 
 const app = express();
 
-const start = async () => {};
+const start = async () => {
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL: ${cms.getAdminURL()}`);
+      },
+    },
+  });
+};
 
 start();
