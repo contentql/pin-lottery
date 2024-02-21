bd691fe4639eb30bdb887a089197912c1fb9ccae
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-02-21 13:47:16 +0530

    chore: added contest page metadata

--- src/app/contest/page.tsx
+++ src/app/contest/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from 'next';
+
 import ContestView from '@/views/ContestView';
+
+export const metadata: Metadata = {
+  title: 'Contest',
+  description: 'This is a contest page',
+};
 
 const Contest = () => {
   return <ContestView />;
