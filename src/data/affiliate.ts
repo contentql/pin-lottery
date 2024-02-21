f0e8317a0d4fdb76e946da438e2d6a259b64f902
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-02-21 13:08:34 +0530

    init: data files

---
+++ src/data/affiliate.ts
@@ -0,0 +1,85 @@
+import client_1 from '/public/images/client/1.png';
+import client_2 from '/public/images/client/2.png';
+import client_3 from '/public/images/client/3.png';
+import client_4 from '/public/images/client/4.png';
+import affiliate_1 from '/public/images/icon/affiliate/1.png';
+import affiliate_2 from '/public/images/icon/affiliate/2.png';
+import affiliate_3 from '/public/images/icon/affiliate/3.png';
+import affiliate_4 from '/public/images/icon/affiliate/4.png';
+import how_work_1 from '/public/images/icon/how-work/1.png';
+import how_work_2 from '/public/images/icon/how-work/2.png';
+import how_work_3 from '/public/images/icon/how-work/3.png';
+import top_affiliate_1 from '/public/images/top-affiliate/1.jpg';
+import top_affiliate_2 from '/public/images/top-affiliate/2.jpg';
+import top_affiliate_3 from '/public/images/top-affiliate/3.jpg';
+
+export const how_it_work = [
+  {
+    id: 1,
+    title: 'SIGN UP',
+    desc: 'Register with us as an affiliate in just a few easy steps',
+    icon: how_work_1,
+  },
+  {
+    id: 2,
+    title: 'PROMOTE',
+    desc: 'Get links or custom affiliate links we provide',
+    icon: how_work_2,
+  },
+  {
+    id: 3,
+    title: 'EARN',
+    desc: 'You receive commission on every refferal',
+    icon: how_work_3,
+  },
+];
+
+export const affiliate_partner = [
+  {
+    id: 1,
+    title: 'No Fees',
+    desc: 'Lorem ipsum dolor sit amet, consectetur eget varius diameget.',
+    icon: affiliate_1,
+  },
+  {
+    id: 2,
+    title: 'Easy Payouts',
+    desc: 'Lorem ipsum dolor sit amet, consectetur eget varius diameget.',
+    icon: affiliate_2,
+  },
+  {
+    id: 3,
+    title: 'Tools For Success',
+    desc: 'Lorem ipsum dolor sit amet, consectetur eget varius diameget.',
+    icon: affiliate_3,
+  },
+  {
+    id: 4,
+    title: '24/7 Support',
+    desc: 'Lorem ipsum dolor sit amet, consectetur eget varius diameget.',
+    icon: affiliate_4,
+  },
+];
+
+export const top_affiliate = [
+  {
+    id: 1,
+    name: 'Ricky Moran',
+    earn: '5,026.66',
+    img: top_affiliate_1,
+  },
+  {
+    id: 2,
+    name: 'Ken Harper',
+    earn: '5,026.66',
+    img: top_affiliate_2,
+  },
+  {
+    id: 3,
+    name: 'Lewis Frank',
+    earn: '5,026.66',
+    img: top_affiliate_3,
+  },
+];
+
+export const clients = [client_1, client_2, client_3, client_4, client_1];
---
+++ src/data/faqData.ts
@@ -0,0 +1,37 @@
+const faqData = [
+  {
+    id: 1,
+    question: 'You Have Questions',
+    answer:
+      "Do not hesitate to send us an email if you can't find what you're looking for.",
+  },
+  {
+    id: 2,
+    question: 'Can I cancel my ticket?',
+    answer: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum dignissimos consectetur aspernatur expedita aut reiciendis magni tempore ullam libero, voluptate nam accusamus est a
+        debitis, obcaecati beatae possimu distinctio?`,
+  },
+  {
+    id: 3,
+    question: ' Can I enter promotions if I buy my tickets using Rifa lottery?',
+    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra
+        maecenas accumsan lacus vel facilisis.`,
+  },
+  {
+    id: 4,
+    question: 'What are the different ticket types?',
+    answer: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum dignissimos consectetur
+        aspernatur expedita aut reiciendis magni tempore ullam libero, voluptate nam accusamus est a
+        debitis, obcaecati beatae possimus veniam distinctio?`,
+  },
+  {
+    id: 5,
+    question: 'What are the draw close off times?',
+    answer: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum dignissimos consectetur
+        aspernatur expedita aut reiciendis magni tempore ullam libero, voluptate nam accusamus est a
+        debitis, obcaecati beatae possimus veniam distinctio?`,
+  },
+];
+
+export default faqData;
