72587ea1f011a158c8a42b5e88895ac1e309e6ca
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-02-21 11:43:08 +0530

    chore: removing module css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
 import Providers from '@/providers/Providers';
 
 const inter = Inter({ subsets: ['latin'] });
