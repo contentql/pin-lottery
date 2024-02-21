2e60163f577f96c851b396bde6fd8f6db5624920
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-02-21 15:37:02 +0530

    chore: updated the layout with styles and bootstrap

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { PropsWithChildren, useState } from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { trpc } from '@/trpc/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
+import { PropsWithChildren, useState } from 'react';
 
 const Providers = ({ children }: PropsWithChildren) => {
   const [queryClient] = useState(() => new QueryClient());
@@ -20,7 +20,7 @@ const Providers = ({ children }: PropsWithChildren) => {
           },
         }),
       ],
-    }),
+    })
   );
 
   return (
