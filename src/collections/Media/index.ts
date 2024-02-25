e1533bfbff2ed97f9781f51eafa494bff3db020c
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-02-25 19:54:06 +0530

    S3 (#11)
    * s3: init
    
    * chore: s3 sdk as resolutions

---
+++ src/collections/Media/index.ts
@@ -0,0 +1,71 @@
+import type { CollectionConfig, Field } from 'payload/types';
+
+const urlField: Field = {
+  name: 'url',
+  type: 'text',
+};
+
+export const Media: CollectionConfig = {
+  slug: 'media',
+  upload: {
+    imageSizes: [
+      {
+        height: 400,
+        width: 400,
+        crop: 'center',
+        name: 'square',
+      },
+      {
+        width: 900,
+        height: 450,
+        crop: 'center',
+        name: 'sixteenByNineMedium',
+      },
+      {
+        name: 'thumbnail',
+        width: 400,
+        height: 300,
+        position: 'centre',
+      },
+      {
+        name: 'card',
+        width: 768,
+        height: 1024,
+        position: 'centre',
+      },
+      {
+        name: 'tablet',
+        width: 1024,
+        // By specifying `undefined` or leaving a height undefined,
+        // the image will be sized to a certain width,
+        // but it will retain its original aspect ratio
+        // and calculate a height automatically.
+        height: undefined,
+        position: 'centre',
+      },
+    ],
+    focalPoint: false,
+    crop: false,
+  },
+  fields: [
+    {
+      name: 'alt',
+      label: 'Alt Text',
+      type: 'text',
+    },
+
+    // The following fields should be able to be merged in to default upload fields
+    urlField,
+    {
+      name: 'sizes',
+      type: 'group',
+      fields: [
+        {
+          name: 'square',
+          type: 'group',
+          fields: [urlField],
+        },
+      ],
+    },
+  ],
+};
---
+++ src/plugins/s3.ts
@@ -0,0 +1,13 @@
+import { s3Adapter } from '@payloadcms/plugin-cloud-storage/s3';
+
+export const s3StorageAdapter = s3Adapter({
+  config: {
+    endpoint: process.env.S3_ENDPOINT,
+    region: process.env.S3_REGION,
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY!,
+      secretAccessKey: process.env.S3_SECRET_KEY!,
+    },
+  },
+  bucket: process.env.S3_BUCKET!,
+});
