ce8255b26644cb6963309141322a5ab633dbef14
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-02-28 16:16:14 +0530

    chore: removed the top nav and updated the main nav with cart and profile

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -43,40 +43,6 @@ const Header = () => {
         windowHeight > 50 && 'menu-fixed animated fadeInDown'
       }`}
     >
-      <div className='header__top'>
-        <div className='container'>
-          <div className='row align-items-center gap-2 gap-md-0'>
-            <div className='col-sm-6'>
-              <div className='left d-flex align-items-center'>
-                <a
-                  href='tel:65655655'
-                  className='d-flex align-items-center gap-1'
-                >
-                  <i className='las la-phone-volume'></i>
-                  Customer Support
-                </a>
-              </div>
-            </div>
-            <div className='col-sm-6'>
-              <div className='right'>
-                <div className='product__cart'>
-                  <span className='total__amount'>0.00</span>
-                  <Link href='/cart' className='amount__btn'>
-                    <i className='las la-shopping-basket'></i>
-                    <span className='cart__num'>{cartData.length}</span>
-                  </Link>
-                </div>
-                <Link
-                  href='/login'
-                  className='user__btn d-flex align-items-center justify-content-center'
-                >
-                  <i className='las la-user'></i>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
       <div className='header__bottom'>
         <div className='container'>
           <nav className='navbar navbar-expand-xl p-0 align-items-center'>
@@ -134,6 +100,22 @@ const Header = () => {
                 <Link href='/contest' className='cmn-btn style--three btn--sm'>
                   <Image src={tag} alt='icon' className='me-1' /> Buy Tickets
                 </Link>
+
+                <div className='right'>
+                  <div className='product__cart'>
+                    <span className='total__amount'>0.00</span>
+                    <Link href='/cart' className='amount__btn'>
+                      <i className='las la-shopping-basket'></i>
+                      <span className='cart__num'>{cartData.length}</span>
+                    </Link>
+                  </div>
+                  <Link
+                    href='/login'
+                    className='user__btn d-flex align-items-center justify-content-center'
+                  >
+                    <i className='las la-user'></i>
+                  </Link>
+                </div>
               </div>
             </div>
           </nav>
