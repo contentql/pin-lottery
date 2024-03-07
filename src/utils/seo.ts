9611f1969d553201efa8f4265db877201342f9b7
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-03-07 15:03:19 +0530

    chore: updated the auto geneartes functins of seo

--- src/utils/seo.ts
+++ src/utils/seo.ts
@@ -7,17 +7,25 @@ import {
 import { envConfig } from '../../env.config'
 
 export const generateTitle: GenerateTitle = (data: any) => {
-  return `${data?.slug || 'lottery'} - ${data?.doc?.title.value || ''}`
+  const title = `${data?.slug || 'lottery'} - ${data?.doc?.title.value || ''}`
+
+  return title
 }
 
 export const generateDescription: GenerateDescription = (data: any) => {
-  return `${data?.doc?.short_desc?.value || ''}`
+  const description = `${data?.doc?.short_desc?.value || ''}`
+
+  return description
 }
 
 export const generateImage: GenerateImage = (data: any) => {
-  return `${data?.doc?.img?.value || ''}`
+  const image = `${data?.doc?.img?.value || ''}`
+
+  return image
 }
 
 export const generateURL: GenerateURL = (data: any) => {
-  return `${envConfig.SERVER_URL}/${data?.locale ? data?.locale + '/' : ''}${data?.slug || ''}`
+  const url = `${envConfig.SERVER_URL}/${data?.locale ? data?.locale + '/' : ''}${data?.slug || ''}/${data?.id || ''}`
+
+  return url
 }
