df7225e207ba910909a9647460e0b98cbe67e2e0
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-03-20 21:45:34 +0530

    chore: separated hooks into files

--- src/collections/Contest/index.ts
+++ src/collections/Contest/index.ts
@@ -4,6 +4,7 @@ import {
   lexicalHTML,
 } from '@payloadcms/richtext-lexical'
 import { CollectionConfig } from 'payload/types'
+import { updateContestAfterRead } from './hooks/updateContestAfterRead'
 
 const Contest: CollectionConfig = {
   slug: 'contest',
@@ -11,49 +12,7 @@ const Contest: CollectionConfig = {
     useAsTitle: 'title',
   },
   hooks: {
-    beforeRead: [
-      async ({ req, doc, context }) => {
-        const { payload } = req
-
-        const { totalDocs: tickets_purchased } = await payload.find({
-          collection: 'tickets',
-          depth: 0,
-          where: {
-            'contest_id.value': {
-              equals: doc.id,
-            },
-          },
-        })
-
-        if (
-          !context?.updateDoc &&
-          doc.tickets_purchased !== tickets_purchased
-        ) {
-          const { ticket_price, product_price } = doc
-          const reachedThreshold =
-            ticket_price * tickets_purchased >= product_price
-
-          const latestData = {
-            ...doc,
-            tickets_purchased,
-            reached_threshold: reachedThreshold,
-          }
-
-          await payload.update({
-            collection: 'contest',
-            id: doc.id,
-            data: latestData,
-            context: {
-              updateDoc: true,
-            },
-          })
-
-          return latestData
-        } else {
-          return doc
-        }
-      },
-    ],
+    beforeRead: [updateContestAfterRead],
   },
   fields: [
     {
--- src/collections/Ticket/index.ts
+++ src/collections/Ticket/index.ts
@@ -1,6 +1,7 @@
 import { CollectionConfig } from 'payload/types'
-import { Contest } from '../../payload-types'
 import { isAdminOrSelf } from './access/isAdminOrSelf'
+import { updateContestAfterCreate } from './hooks/updateContestAfterCreate'
+import { updateContestAfterDelete } from './hooks/updateContestAfterDelete'
 
 const Ticket: CollectionConfig = {
   slug: 'tickets',
@@ -14,60 +15,8 @@ const Ticket: CollectionConfig = {
   },
   // when creating a ticket, ensure the button was disabled
   hooks: {
-    afterChange: [
-      async ({ operation, doc, req }) => {
-        const { payload } = req
-        // read total tickets and updated in contest
-        // check threshold and update it
-
-        if (operation === 'create') {
-          const { totalDocs: tickets_purchased, docs: tickets } =
-            await payload.find({
-              collection: 'tickets',
-              depth: 1,
-              where: {
-                'contest_id.value': {
-                  equals: req.body.contest_id.value,
-                },
-              },
-            })
-
-          const ticket = tickets.find(ticket => ticket.id == doc.id)
-
-          const product_price = (ticket?.contest_id?.value as Contest)
-            ?.product_price
-
-          const {
-            contest_id: { value: id },
-            ticket_price,
-          } = doc
-
-          const reached_threshold =
-            ticket_price * tickets_purchased >= product_price
-
-          const latestData = {
-            ...doc,
-            tickets_purchased,
-            reached_threshold,
-          }
-
-          await payload.update({
-            collection: 'contest',
-            id,
-            data: latestData,
-          })
-        }
-      },
-    ],
-    afterOperation: [
-      async ({ result, req, operation }) => {
-        if (operation === 'delete' || operation === 'deleteByID') {
-          const { payload } = req
-          // read total tickets and
-        }
-        return result
-      },
-    ],
+    afterChange: [updateContestAfterCreate],
+    afterOperation: [updateContestAfterDelete],
   },
   fields: [
     {
