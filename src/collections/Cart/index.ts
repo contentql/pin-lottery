1a5c0231482320e82606e9a9dde44e8e42ff09b8
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-03-25 18:07:30 +0530

    chore: removing cart from admin panel

--- src/collections/Cart/index.ts
+++ src/collections/Cart/index.ts
@@ -5,6 +5,17 @@ import { assignUserId } from './field-level-hooks/assignUserId'
 
 const Cart: CollectionConfig = {
   slug: 'cart',
+  admin: {
+    hidden: ({ user }) => {
+      const { email, id } = user // only email and id are available here
+
+      if (process.env.NODE_ENV !== 'production') {
+        return true
+      }
+
+      return false
+    },
+  },
   access: {
     read: isAdminOrSelf,
     update: isAdminOrSelf,
