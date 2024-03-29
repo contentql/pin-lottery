6e155322d71472a0608548379b8f75c570b86fe1
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-03-29 23:22:09 +0530

    chore: updated access

--- src/collections/Users/access/isAdminOrSelf.ts
+++ src/collections/Users/access/isAdminOrSelf.ts
@@ -2,9 +2,9 @@ import { Access } from 'payload/config'
 
 export const isAdminOrSelf: Access = ({ req: { user } }) => {
   if (user) {
-    if (user.roles?.includes('admin')) {
-      return true
-    }
+    const { roles } = user
+
+    if (roles?.includes('admin')) return true
 
     return {
       id: {
