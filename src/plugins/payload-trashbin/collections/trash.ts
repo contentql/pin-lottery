5af61d8c9e51049b6e7a81ef3e1ce5d3ce3e8cf6
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-04-01 22:54:11 +0530

    chore: removing title to focus more on id

--- src/plugins/payload-trashbin/collections/trash.ts
+++ src/plugins/payload-trashbin/collections/trash.ts
@@ -4,13 +4,11 @@ import { RestoreButton } from '../components/RestoreButton'
 
 export const Trash: CollectionConfig = {
   slug: 'trash',
-  admin: {
-    useAsTitle: 'collectionName',
-  },
   access: {
     create: () => false,
     update: () => false,
   },
+
   fields: [
     {
       name: 'value',
