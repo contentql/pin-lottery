0c5da2b7714fa3d87e9c2efc14074239d6f64ee5
jagadeeesh507 <jagadeeshm778@gmail.com>
2024-04-04 17:16:29 +0530

    chore: similar contests updated

--- src/views/ContestDetailsView.tsx
+++ src/views/ContestDetailsView.tsx
@@ -87,23 +87,26 @@ const ContestDetailsView = ({ contestId }: PageProps) => {
       )}
       {isSimilarContestsPending ? (
         <>
-          {' '}
-          <div className='section-header text-center'>
-            <h2 className=''>Similar Contests</h2>
-          </div>
-          <div
-            className='tab-pane fade show active '
-            id='dream'
-            role='tabpanel'
-            aria-labelledby='dream-tab'>
-            <div className='row mb-none-30 mt-50'>
-              {[1, 2, 3].map((ele, index) => (
-                <div key={index} className='col-xl-4 col-md-6 mb-30'>
-                  <ContestSkeletons />
+          <section className='pb-120 mt-minus-100'>
+            <div className='container'>
+              <div className='section-header text-center'>
+                <h2 className=''>Similar Contests</h2>
+              </div>
+              <div
+                className='tab-pane fade show active '
+                id='dream'
+                role='tabpanel'
+                aria-labelledby='dream-tab'>
+                <div className='row mb-none-30 mt-50'>
+                  {[1, 2, 3].map((ele, index) => (
+                    <div key={index} className='col-xl-4 col-md-6 mb-30'>
+                      <ContestSkeletons />
+                    </div>
+                  ))}
                 </div>
-              ))}
+              </div>
             </div>
-          </div>
+          </section>
         </>
       ) : (
         similarContest?.length! > 0 && (
