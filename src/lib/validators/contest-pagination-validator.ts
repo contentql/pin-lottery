891cc3dcdf4efc08edaf4cd17f0df51faedd56a2
jagadeeesh507 <jagadeeshm778@gmail.com>
2024-04-04 16:58:43 +0530

    chore: empty state for contest and filter buy contest status updated

--- src/lib/validators/contest-pagination-validator.ts
+++ src/lib/validators/contest-pagination-validator.ts
@@ -4,6 +4,7 @@ export const ContestPaginationValidator = z.object({
   pageNumber: z.number(),
   filterByName: z.string(),
   filterByPrice: z.number(),
+  filterByContestStatus: z.string(),
   filterByTitle: z.string(),
 })
 
