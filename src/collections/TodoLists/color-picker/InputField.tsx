9010cce817b2cff2d79b90bf2956e97c76e532e2
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-04-04 15:00:37 +0530

    init: custom color field

---
+++ src/collections/TodoLists/color-picker/InputField.tsx
@@ -0,0 +1,153 @@
+import React, { Fragment, useCallback, useEffect, useState } from 'react'
+
+// this is how we'll interface with Payload itself
+import { useFieldType } from 'payload/components/forms'
+
+// retrieve and store the last used colors of your users
+import { usePreferences } from 'payload/components/preferences'
+
+// re-use Payload's built-in button component
+import { Button } from 'payload/components'
+
+// we'll re-use the built in Label component directly from Payload
+import { Label } from 'payload/components/forms'
+
+import Error from 'payload/dist/admin/components/forms/Error/index'
+
+// we can use existing Payload types easily
+import { Props } from 'payload/components/fields/Text'
+
+// we'll import and reuse our existing validator function on the frontend, too
+import { validateHexColor } from './config'
+
+// Import the SCSS stylesheet
+import './styles.scss'
+
+// keep a list of default colors to choose from
+const defaultColors = [
+  '#333333',
+  '#9A9A9A',
+  '#F3F3F3',
+  '#FF6F76',
+  '#FDFFA4',
+  '#B2FFD6',
+  '#F3DDF3',
+]
+const baseClass = 'custom-color-picker'
+
+const preferenceKey = 'color-picker-colors'
+
+const InputField: React.FC<Props> = props => {
+  const { path, label, required, validate } = props
+
+  const {
+    value = '',
+    setValue,
+    errorMessage,
+    showError,
+  } = useFieldType({
+    path,
+    validate,
+  })
+  const classes = ['field-type', 'text', baseClass, showError && 'error']
+    .filter(Boolean)
+    .join(' ')
+
+  const { getPreference, setPreference } = usePreferences()
+  const [colorOptions, setColorOptions] = useState(defaultColors)
+  const [isAdding, setIsAdding] = useState(false)
+  const [colorToAdd, setColorToAdd] = useState('')
+
+  useEffect(() => {
+    const mergeColorsFromPreferences = async () => {
+      const colorPreferences = await getPreference<string[]>(preferenceKey)
+      if (colorPreferences) {
+        setColorOptions(colorPreferences)
+      }
+    }
+    mergeColorsFromPreferences()
+  }, [getPreference, setColorOptions])
+
+  const handleAddColor = useCallback(() => {
+    setIsAdding(false)
+    setValue(colorToAdd)
+
+    // prevent adding duplicates
+    if (colorOptions.indexOf(colorToAdd) > -1) return
+
+    let newOptions = colorOptions
+    newOptions.unshift(colorToAdd)
+
+    // update state with new colors
+    setColorOptions(newOptions)
+    // store the user color preferences for future use
+    setPreference(preferenceKey, newOptions)
+  }, [colorOptions, setPreference, colorToAdd, setIsAdding, setValue])
+
+  return (
+    <div className={classes}>
+      <Label htmlFor={path} label={label} required={required} />
+      <Error showError={showError} message={errorMessage!} />
+      {isAdding && (
+        <div>
+          <input
+            className={`${baseClass}__input`}
+            type='text'
+            placeholder='#000000'
+            onChange={e => setColorToAdd(e.target.value)}
+            value={colorToAdd}
+          />
+          <Button
+            className={`${baseClass}__btn`}
+            buttonStyle='primary'
+            iconPosition='left'
+            iconStyle='with-border'
+            size='small'
+            onClick={handleAddColor}
+            disabled={validateHexColor(colorToAdd) !== true}>
+            Add
+          </Button>
+          <Button
+            className={`${baseClass}__btn`}
+            buttonStyle='secondary'
+            iconPosition='left'
+            iconStyle='with-border'
+            size='small'
+            onClick={() => setIsAdding(false)}>
+            Cancel
+          </Button>
+        </div>
+      )}
+      {!isAdding && (
+        <Fragment>
+          <ul className={`${baseClass}__colors ${showError ? 'error' : ''}`}>
+            {colorOptions.map((color, i) => (
+              <li key={i}>
+                <button
+                  type='button'
+                  key={color}
+                  className={`chip ${color === value ? 'chip--selected' : ''} chip--clickable`}
+                  style={{ backgroundColor: color }}
+                  aria-label={color}
+                  onClick={() => setValue(color)}
+                />
+              </li>
+            ))}
+          </ul>
+          <Button
+            className='add-color'
+            icon='plus'
+            buttonStyle='icon-label'
+            iconPosition='left'
+            iconStyle='with-border'
+            onClick={() => {
+              setIsAdding(true)
+              setValue('')
+            }}
+          />
+        </Fragment>
+      )}
+    </div>
+  )
+}
+export default InputField
---
+++ src/collections/TodoLists/index.ts
@@ -0,0 +1,40 @@
+import { CollectionConfig } from 'payload/types'
+import colorField from './color-picker/config'
+
+const Todo: CollectionConfig = {
+  slug: 'todos',
+  admin: {
+    defaultColumns: ['listName', 'tasks', 'updatedAt'],
+    useAsTitle: 'listName',
+  },
+  access: {
+    create: () => true,
+    read: () => true,
+    update: () => true,
+    delete: () => true,
+  },
+  fields: [
+    {
+      name: 'listName',
+      type: 'text',
+    },
+    colorField,
+    {
+      name: 'tasks',
+      type: 'array',
+      fields: [
+        {
+          name: 'name',
+          type: 'text',
+        },
+        {
+          name: 'complete',
+          type: 'checkbox',
+          defaultValue: false,
+        },
+      ],
+    },
+  ],
+}
+
+export default Todo
