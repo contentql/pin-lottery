a4e04f286cc04471c4830641d097947c207a84c9
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-04-05 17:54:55 +0530

    chore: updated all the dashboard pages with layout of left menu

--- src/views/UserTransactionView.tsx
+++ src/views/UserTransactionView.tsx
@@ -1,24 +1,12 @@
 'use client'
 
-import LeftSideMenu from '@/components/common/LeftSideMenu'
 import RightSilde from '@/components/user-transaction/RightSilde'
 
 const UserTransactionView = () => {
   return (
     <>
-      <div className='inner-hero-section style--five'></div>
-
-      <div className='mt-minus-150 pb-120'>
-        <div className='container'>
-          <div className='row'>
-            {/* TODO: suer info */}
-            <LeftSideMenu />
-
-            {/* Right side here */}
-            <RightSilde />
-          </div>
-        </div>
-      </div>
+      {/* Right side here */}
+      <RightSilde />
     </>
   )
 }
