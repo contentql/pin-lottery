5c0349e5682f784a1c4455af6305f8aa35d5e21c
ABDUL RAHAMAN <36rahaman@gmail.com>
2024-04-05 15:50:21 +0530

    chore: added metadata for got marketing routes

--- src/app/(marketing)/(protected)/cart/page.tsx
+++ src/app/(marketing)/(protected)/cart/page.tsx
@@ -5,6 +5,14 @@ import CartView from '@/views/CartView'
 export const metadata: Metadata = {
   title: 'Cart',
   description: 'this is cart page',
+  openGraph: {
+    title: 'Cart',
+    description: 'This is a cart page',
+    images: [`/images/icon/overview/2.png`],
+    type: 'website',
+    locale: 'en_US',
+    url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cart`,
+  },
 }
 
 const Cart = async () => {
