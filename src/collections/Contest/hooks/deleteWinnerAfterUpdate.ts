51addc6ebb4f2a5ef5eb90f34457b805a0732366
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-04-15 12:19:27 +0530

    chore: updated the deleteWinnerAfterUpdate

--- src/collections/Contest/hooks/deleteWinnerAfterUpdate.ts
+++ src/collections/Contest/hooks/deleteWinnerAfterUpdate.ts
@@ -12,12 +12,6 @@ export const deleteWinnerAfterUpdate: CollectionAfterChangeHook = async ({
 
       const { winner_ticket } = previousDoc
 
-      // const winner = await payload.findByID({
-      //   collection: 'winner', 
-      //   id: winner_ticket?.value.id || winner_ticket?.value
-      // })
-
-      // console.log("winner: ", winner)
       // Check if there was a winner ticket
       if (!winner_ticket) {
         const latestData = {
@@ -34,7 +28,6 @@ export const deleteWinnerAfterUpdate: CollectionAfterChangeHook = async ({
           })
         } catch (error) {
           console.error('Error updating contest: ', error)
-          // throw new Error('Failed to update contest.')
         }
 
         return
@@ -49,24 +42,24 @@ export const deleteWinnerAfterUpdate: CollectionAfterChangeHook = async ({
         })
       } catch (error: any) {
         console.error('Error deleting winner document: ', error)
-        // throw new Error('Failed to delete winner document.')
-        if(error.status === 404)
-        {const latestData = {
-          contest_timer_status: false,
-          contest_status: false,
-          winner_ticket: null,
-        }
 
-        try {
-          await payload.update({
-            collection: 'contest',
-            id: doc?.id,
-            data: { ...latestData },
-          })
-        } catch (error) {
-          console.error('Error updating contest: ', error)
-          // throw new Error('Failed to update contest.')
-        }}
+        if (error.status === 404) {
+          const latestData = {
+            contest_timer_status: false,
+            contest_status: false,
+            winner_ticket: null,
+          }
+
+          try {
+            await payload.update({
+              collection: 'contest',
+              id: doc?.id,
+              data: { ...latestData },
+            })
+          } catch (error) {
+            console.error('Error updating contest: ', error)
+          }
+        }
       }
     }
   }
