c9b36a680687e8cd84677acfd8c28af3e1139767
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-04-19 09:40:46 +0530

    chore: custom callback and removed deepsource

--- src/plugins/payload-paystack/handlers/create-paystack-checkout-url.ts
+++ src/plugins/payload-paystack/handlers/create-paystack-checkout-url.ts
@@ -1,3 +1,4 @@
+import { PluginTypes } from '../types'
 import e from 'express'
 import { PayloadRequest } from 'payload/types'
 
@@ -5,12 +6,15 @@ const createPaystackCheckoutUrl = async (
   req: PayloadRequest,
   res: e.Response<any, Record<string, any>>,
   paystackSdk: any,
+  pluginOptions: PluginTypes,
 ) => {
   const { body, user } = req
+  const { frontend_url } = pluginOptions
 
   const checkout = await paystackSdk.transaction.initialize({
     amount: String(Number(body.depositAmount) * 100),
     email: user.email,
+    callback_url: `${frontend_url}/${body.callback_url}`,
   })
 
   if (!checkout.status) {
