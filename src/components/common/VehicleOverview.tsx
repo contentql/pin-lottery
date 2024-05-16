7eff8ec277b761076c466f92aea2feee89a2f8fe
jagadeesh <jagadeeshm778@gmail.com>
2024-05-16 10:47:59 +0530

    chore: remove specification and features and direcly display the description,

--- src/components/common/VehicleOverview.tsx
+++ src/components/common/VehicleOverview.tsx
@@ -1,29 +1,28 @@
 import { Contest } from '@/payload-types'
 import ConvertToHtml from '@/utils/convertToHtml'
 
-import Tabs from './Tabs'
-import VehicleSpecifications from './VehicleSpecifications'
 
 const VehicleOverview = ({ contestDetails }: { contestDetails: Contest }) => {
-  const tabs = [
-    {
-      title: 'Specifications',
-      content: <VehicleSpecifications contestDetails={contestDetails} />,
-    },
-    {
-      title: 'Description',
-      content: (
-        <ConvertToHtml htmlContent={contestDetails?.description_html!} />
-      ),
-    },
-    {
-      title: 'Features',
-      content: <ConvertToHtml htmlContent={contestDetails?.features_html!} />,
-    },
-  ]
+  // const tabs = [
+  //   {
+  //     title: 'Specifications',
+  //     content: <VehicleSpecifications contestDetails={contestDetails} />,
+  //   },
+  //   {
+  //     title: 'Description',
+  //     content: (
+  //       <ConvertToHtml htmlContent={contestDetails?.description_html!} />
+  //     ),
+  //   },
+  //   {
+  //     title: 'Features',
+  //     content: <ConvertToHtml htmlContent={contestDetails?.features_html!} />,
+  //   },
+  // ]
   return (
     <div>
-      <Tabs tabs={tabs} isTicketsPending={false} />
+      <h2 className='product-description'>Description:</h2>
+       <ConvertToHtml htmlContent={contestDetails?.description_html!} />
     </div>
   )
 }
