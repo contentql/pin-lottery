5544de7992c5565a2855513a08d997f539efcb08
jagadeesh <jagadeeshm778@gmail.com>
2024-05-21 09:36:51 +0530

    chore: before dashboard updated

--- src/payload-components/BeforeDashboard/index.tsx
+++ src/payload-components/BeforeDashboard/index.tsx
@@ -1,8 +1,8 @@
+import { useAuth } from 'payload/components/utilities'
+import React from 'react'
 import RemainingWalletAmount from '../RemainingWalletAmount'
 import TotalTicketsPurchased from '../TotalTicketsPurchased'
 import Transactions from '../Transactions'
-import { useAuth } from 'payload/components/utilities'
-import React from 'react'
 
 const BeforeDashboard = () => {
   const test = React.useState()
@@ -13,11 +13,7 @@ const BeforeDashboard = () => {
     return (
       <div>
         <h2>Overview of Application</h2>
-        <p style={{ color: 'darkorange' }}>
-          This feature is still under process, more cards to come!
-        </p>
         <div className='dashboard__group'>
-          <h2 className='dashboard__label'>Custom Components</h2>
           <ul className='dashboard__card-list'>
             <TotalTicketsPurchased />
             <Transactions />
