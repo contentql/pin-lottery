00fb2668eed27300b91565577a515e604bdbd4f5
jagadeesh <jagadeeshm778@gmail.com>
2024-05-24 14:08:12 +0530

    chore: consoles removed

--- src/collections/Users/hooks/verifyUserEmail.ts
+++ src/collections/Users/hooks/verifyUserEmail.ts
@@ -7,9 +7,6 @@ export const verifyUserEmail: CollectionAfterChangeHook = async ({
 }) => {
   if (operation === 'create') {
    const user=await req.payload.db.collections['users'].findOne({ _id: doc.id})
-    console.log('fetched user',user,doc.id)
-    console.log("console user after create",doc)
-
     await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/trpc/message.verifyEmail`,{
       method:'POST',
       headers: {
