8b2f03a430fdb0cf5ceb5e5c3f6784dd2a5c5406
jagadeesh <jagadeeshm778@gmail.com>
2024-07-02 10:55:09 +0530

    chore: updated

--- src/collections/Users/hooks/verifyUserEmail.ts
+++ src/collections/Users/hooks/verifyUserEmail.ts
@@ -6,13 +6,18 @@ export const verifyUserEmail: CollectionAfterChangeHook = async ({
   doc,
 }) => {
   if (operation === 'create') {
-   const user=await req.payload.db.collections['users'].findOne({ _id: doc.id})
-    await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/trpc/message.verifyEmail`,{
-      method:'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body:JSON.stringify({token:user._verificationToken})
+    const user = await req.payload.db.collections['users'].findOne({
+      _id: doc.id,
     })
+    await fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/api/trpc/message.verifyEmail`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token: user?._verificationToken }),
+      },
+    )
   }
 }
